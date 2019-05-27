d1741a978c7157ef166c9e339a66ac827687ca11
Saurav Kumar <sauravk7077@gmail.com>
2019-05-27 07:57:18 +0530

    Added some code.

---
+++ comments_and_loop_exit.py
@@ -0,0 +1,33 @@
+#This stops a loop in between using break
+k = 5
+for j in range(100,50,-1):
+    if j % k is 0:
+        print("This is number divisible by",k,"is",j)
+        break
+
+#This is a single line comment
+
+'''
+    This is a
+    multiple
+    line
+    comment
+'''
+
+#This skips the divisible numbers and prints others
+k = 7
+for r in range(1,100):
+    if r%k is 0:
+        continue
+    else:
+        print(r)
+
+#This prints multiples of 4 which are less than 100
+for r in range(1,100):
+    if r%4 is 0:
+        print(r)
+'''
+#This is a better way
+for i in range(4,100,4):
+    print(i)
+'''
